20cdd8edb6cb23e4d94e7afc7189c4d1083e2bc8
Guillian <guillianbillard@gmail.com>
2023-01-11 09:56:35 +0100

    finished nbBits and Hanoi

--- HanoiRules.py
+++ HanoiRules.py
@@ -1,21 +1,38 @@
+from copy import deepcopy, copy
+
 from transitionRelation import TransitionRelation
-from HanoiConfig import HanoiConfig
+
+
 class HanoiRules(TransitionRelation):
 
-        def __init__(self,n,k):
-            self.n = n;
-            self.hanoiConfig = HanoiConfig(n,k)
-            self.initial = [[1,2,3],[],[]]
+    def __init__(self):
+        self.initial = [[1,2,3], [], []]
 
-        def next(self, source):
-            all_config = []
-            for i in source:
-                for j in source:
-                    #si la selection (i,j) correspond à deux tours distinctes:
-                    if len(i) != 0:
-                        if len(j) == 0 or i[0] < j[0]:
+    def next(self, source):
+        # from a source configuration, compute all possibles next configurations
+        all_config = []
+        for i in range(0, 3):
+            for j in range(0, 3):
+                config = deepcopy(source)
+                if i != j:
+                    if len(config[i]) != 0:
+                        if len(config[j]) == 0:
+                            config[j].insert(0, config[i].pop(0))
+                            all_config.append(config)
+                        elif config[i][len(config[i]) - 1] < config[j][len(config[j]) - 1]:
+                            config[j].insert(0,config[i].pop(0))
+                            all_config.append(config)
+        return all_config
+
+    def roots(self):
+        return self.initial
 
 
-        def
-        """def roots(self):"""
+if __name__ == '__main__':
+    hanoi = HanoiRules()
+    print(hanoi.roots())
+    for res in hanoi.next(hanoi.roots()):
+        print("\t" + str(res))
+        for res2 in hanoi.next(res):
+            print("\t"*2 + str(res2))
 
---
+++ nbBits.py
@@ -0,0 +1,33 @@
+from copy import deepcopy
+
+from transitionRelation import TransitionRelation
+
+
+class NbBits(TransitionRelation):
+    def flipnthbit(self, n, k):
+        return n ^ (1 << k)
+
+    def roots(self):
+        return 0
+
+    def next(self, source):
+        result = []
+        for i in range(0, self.k):
+            candidate = deepcopy(source)
+            candidate = self.flipnthbit(candidate, i)
+            result.append(candidate)
+        return result
+
+    def __init__(self, k):
+        self.k = k
+
+
+# main
+if __name__ == '__main__':
+    k = 6
+    nb = NbBits(k)
+    for res in nb.next(nb.roots()):
+        print(format(res, '#0{}b'.format(k + 2)))
+        for res2 in nb.next(res):
+            print("\t" + format(res2, '#0{}b'.format(k + 2)))
+
