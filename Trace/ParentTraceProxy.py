e61e3e64ed7b1f463f5f0bb342f7ad8bd69dab95
Guillian <guillianbillard@gmail.com>
2023-02-06 15:59:21 +0100

    update ptt

--- Trace/ParentTraceProxy.py
+++ Trace/ParentTraceProxy.py
@@ -28,19 +28,46 @@ class ParentTraceProxy(IdentityProxy):
         return neighbours
 
     def get_trace(self,dic, target):
+        #Attention, ne retourne qu'une trace pour le moment (en passant par le premier parent)
         res = [target] #trace sera la liste res
         courant = target
-        print("Courant = " + str(courant) + ", dic[courant] = " + str(dic[courant]))
-        while courant != dic[courant] :
-            courant = dic[res[-1]] #le dernier de la trace
-            res.append(courant)
+
+        #On remonte la trace en utilisant le dictionnaire reverse jusqu'à arriver à un élément qui n'a pas de parent
+        revdic=self.reverse_graph(dic)
+        #tant que courant a un parent
+        while revdic[courant]:
+            #on ajoute le parent à la trace
+            res.append(revdic[courant][0])
+            #on passe au parent
+            courant = revdic[courant][0]
         return res
+
+
+    def reverse_graph(self, dic):
+        #dic est de la forme (entité, [entités enfants])
+        #retourne un dictionnaire de la forme (entité, [entités parents])
+        #add the entrypoints
+
+        res = {}
+        for k in dic:
+            res[k] = []
+        for k in dic:
+            for v in dic[k]:
+                if v not in res:
+                    res[v] = [k]
+                else:
+                    res[v].append(k)
+        return res
+
+
 
 
 if __name__ == '__main__':
 
     dic = {0: [1,2], 1: [3,4], 2: [5,6], 3: [], 4: [], 5: [], 6: []}
     graph = DicGraph(dic, [0])
+    print(graph.graph)
     ptp = ParentTraceProxy(graph, {})
+    print(ptp.reverse_graph(dic))
 
     print(ptp.get_trace(dic, 6))
