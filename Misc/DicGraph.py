5601033b1f76c1882b14101fb37efcb2c4746256
Guillian <guillianbillard@gmail.com>
2023-02-13 14:37:02 +0100

    pydoc for DicGraph

--- Misc/DicGraph.py
+++ Misc/DicGraph.py
@@ -4,48 +4,58 @@ from Misc.transitionRelation import TransitionRelation
 
 
 class DicGraph(TransitionRelation):
-    '''
+    """
     A graph represented as a dictionary.
-    Exemples:
+    Examples:
     dico = {0: [1,2], 1: [3,4], 2: [5,6], 3: [], 4: [], 5: [], 6: []}
-    Represent the following graph:
+    Represents the following graph:
         0
       |   \
      1     2
     | \   | \
     3 4   5 6
-    '''
+    """
     def __init__(self, dico, roots):
+        """
+        Initialize the graph with the given dictionary and roots.
+        """
         self.graph = dico
         self.entrypoints = roots
 
     def roots(self):
+        """
+        Return the entry points of the graph.
+        """
         return self.entrypoints
 
     def next(self, source):
+        """
+        Return the neighbors of the given source node.
+        """
         try:
             return self.graph[source]
         except KeyError:
             return []
 
-
-
-    '''
-    bfs = Breadth First Search
-    Allows for a breadth first search on a graph represented as a dictionary.
-    Uses a queue to store the nodes to visit and a set to store the nodes already visited.
-    Uses three lambda functions to specify what to do when a node is visited i.e. on_entry, 
-    when a node is already known, i.e. on_known and when the search is finished i.e. on_exit.
-    The lambda functions are called with the following parameters:
-    - source: the node from which the current node was discovered
-    - n: the current node
-    - acc: the accumulator
-    '''
-
     def bfs(self, acc, on_entry=lambda source, n, acc: None,
         on_known=lambda source, n, acc: None,
         on_exit=lambda source, acc: None):
+        """
+        Perform a breadth-first search on the graph represented as a dictionary.
 
+        The search uses a queue to store the nodes to visit and a set to store
+        the nodes already visited. The function uses three lambda functions
+        to specify what to do when a node is visited (`on_entry`), when a node
+        is already known (`on_known`), and when the search is finished (`on_exit`).
+
+        The lambda functions are called with the following parameters:
+        - `source`: the node from which the current node was discovered
+        - `n`: the current node
+        - `acc`: the accumulator
+
+        Returns:
+        A set of the known nodes.
+        """
         knowns = set()
         frontier = deque()
         at_start = True
@@ -68,12 +78,18 @@ class DicGraph(TransitionRelation):
         on_exit(source, acc)
         return knowns
 
-    '''
-    Using bfs, this function allows for predicate finding.'''
-
     def predicate_finder(self, predicate=lambda n: False):
-        #create the accumulator which contains a boolean if the predicate is true at least once, and the list
-        #nodes for which the predicate is true
+        """
+        Find a node in the graph that satisfies the given predicate.
+
+        The function uses the `bfs` method to perform the search and calls
+        the `predicate` function for each node visited to check if it
+        satisfies the given condition.
+
+        Returns:
+        A list containing a boolean indicating whether the predicate is true
+        for at least one node and a list of nodes for which the predicate is true.
+        """
         accumulator = [False, []]
 
         def check_predicate(s, n, a):
